303ec220d1eb21ce7515230aff0a8c13f90f4aef
Harsh Kumar <harshkumar9.in@gmail.com>
2020-04-02 12:58:24 +0530

    templates created

--- Project/settings.py
+++ Project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # own APPS
+    'weather.apps.WeatherConfig',
 ]
 
 MIDDLEWARE = [
