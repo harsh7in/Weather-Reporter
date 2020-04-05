8e37642864b0a237bb400bb817b4756ebe88ecb7
Harsh Kumar <harshkumar9.in@gmail.com>
2020-04-05 16:42:44 +0530

    models updated

--- weather/admin.py
+++ weather/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import city
 
 # Register your models here.
-admin.site.register(city)
--- weather/models.py
+++ weather/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
 class city(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length = 30)
 
-# show the actual city name on the dashboard
+# Show the actual city name on dashboard
     def __str__(self):
         return self.name
-
-#show the plural of city as cities instead of citys
-    class Meta: 
-        verbose_name_plural = 'cities'
+    
+# Show the plural of city as cities instead of citys
+    class Meta:
+        verbose_name_plural='cities'
+        
+    
