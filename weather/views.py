60a911e26cf494392ffdc0b199f28607a6191e8f
Harsh Kumar <harshkumar9.in@gmail.com>
2020-04-05 18:39:17 +0530

    data fatching from database

--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
 import requests
+from .models import city 
 # Create your views here.
 def home(request):
 
     url= 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=f6a2db50ce45d87de65cf404b5b8983e'
-    city= 'China'
+    cities=city.objects.all()
 
-    # request the json data and convert it into Python data_types
-    city_weather=requests.get(url.format(city)).json()
+    weather_data=[]
+    
+    for City in cities:
 
-    # Dictionary for getting api objects
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon']
-    }
+        # request the json data and convert it into Python data_types
+        city_weather=requests.get(url.format(city)).json()
 
+        # Dictionary for getting api objects
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+        weather_data.append(weather)
 
-    context = {'weather' : weather}
+    context = {'weather_data' : weather_data}
     return render(request,"home.html", context)
